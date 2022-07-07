84e4f45e8943f8a150822dea3a4227d1279e48ba
Daniel Mustard <danielmustard1@icloud.com>
2022-07-07 09:41:49 +0100

    Added scoring system and working rock paper scissors

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+//variables for storing player and computer scores
+
+let ps = 0;
+let cs = 0;
+
 
 //Computer play
 function computerPlay (){
@@ -9,29 +14,56 @@ function computerPlay (){
 function playRound(playerSelection, computerSelection){
     //Player winning sections
     if (playerSelection === "rock" && computerSelection === "scissors"){
-        return  ("You win, rock beats scissors!")
+        return  ("player win")
     } else if (playerSelection === "paper" && computerSelection === "rock"){
-        return ("You win paper beats rock!")
+        return ("player win")
     }else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return ("You win Scissors beats paper");
+        return ("player win");
     //Player losing Selections
     }else if (computerSelection === "rock" && playerSelection === "scissors"){
-        return  ("You lose, computer chose rock")
+        return  ("player lose rock")
     }else if (computerSelection === "paper" && playerSelection === "rock"){
-        return ("You lose, computer chose paper")
+        return ("player lose paper")
     }else if (computerSelection === "scissors" && playerSelection === "paper"){
-        return ("You lose, computer chose scissors")
+        return ("player lose scissors")
     //Both choosing same outcome    
     }else if (computerSelection === playerSelection){
-        return (`Draw! you both picked ${computerSelection}`)
+        return ("draw")
     }
 } 
+//console.log(playRound(playerSelection, computerPlay()));
 
 function game(){
-    for (let i = 0; i < 5; i++) {
+    for (let i = 1; i < 6; i++) {
         console.log(`🟦Round ${i}🟦`)
         let playerSelection = prompt("Enter Rock🤘, Paper🧻 or Scissors ✂")
+        let gameResult = playRound(playerSelection, computerPlay());
+        if (gameResult == "player win"){
+            console.log ("You WON this round!")
+            playerScore();
+        }else if (gameResult == "player lose rock"){
+            console.log ("You LOST this round! Computer chose rock!")
+            computerScore(); 
+        }else if (gameResult == "player lose paper"){
+            console.log ("You LOST this round! Computer chose paper!")
+            computerScore();            
+        }else if (gameResult == "player lose paper"){
+            console.log ("You LOST this round! Computer chose scissors!")
+            computerScore();
+        }else if (gameResult == "draw"){
+            console.log(`Draw! you both picked`);
+        }
      }
+     console.log(`🧑Player scored ${ps} | 💻 Computer Scored ${cs}`)
 }
 
-// game();
+function playerScore(){
+    ps++;
+    return ps;
+}
+function computerScore(){
+    cs++;
+    return cs;
+}
+
+game();
